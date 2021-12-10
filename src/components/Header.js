8786b61ca3c3a5ef56a0b334efbdd6a544e6fea4
lazizbek7857 <lazizbekganiyev35@gmail.com>
2021-12-10 21:23:16 +0500

    2

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="left brand-logo">
+          React Shop / SPA
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="collapsible.html">Shop</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Header
---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+function Loader() {
+  return (
+    <div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+  );
+}
+export default Loader;
